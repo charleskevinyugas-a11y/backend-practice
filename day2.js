8d3eceef1964eef74da121a472194bed19833130
Kaysansama <charleskevinyugas@gmail.com>
2026-01-09 09:58:48 +0800

    2nd Day

---
+++ day2.js
@@ -0,0 +1,20 @@
+let name = "Juan";
+let age = 20;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+let monthlyIncome = 10000;
+let savingsRate = 0.2;
+
+let savings = monthlyIncome * savingsRate;
+
+console.log("Monthly savings:", savings);
+
+if (age >= 18) {
+    console.log("Adult");
+} else {
+    console.log("Minor");
+}
